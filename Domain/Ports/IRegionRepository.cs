d06de08189a8b4b909a0db40cbcc8616ccb5d4b1
Isa94d-lab <Isabelagalvis7809@gmail.com>
2025-05-07 09:05:05 -0500

    feat: :sparkles: Repositories

---
+++ Domain/Ports/IRegionRepository.cs
@@ -0,0 +1,9 @@
+using System;
+using GestionInventario.Domain.Entities;
+
+namespace GestionInventario.Domain.Ports;
+
+public interface IRegionRepository : IGenericRepository<Region>
+{
+
+}
--- obj/Debug/net8.0/GestionInventario.AssemblyInfo.cs
+++ obj/Debug/net8.0/GestionInventario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6ab8d56a6fe575021c06385d6c6c20450599a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f78af2431e84dd058c3674c5b437e514a4592d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
